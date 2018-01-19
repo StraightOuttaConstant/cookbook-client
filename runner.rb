ad02213781c6f103a6b26832636b49ea25aac3bf
JackMarx <seriousfools@gmail.com>
2018-01-18 20:05:54 -0600

    create and clean up

--- runner.rb
+++ runner.rb
@@ -1,6 +1,48 @@
 require 'unirest'
 
-response = Unirest.get("http://localhost:3000/one_recipe_url")
-recipe_data = response.body
+system "clear"
 
-puts JSON.pretty_generate(recipe_data)
+puts "Welcome to my Cookbook App"
+puts "make a selection"
+puts "    [1] See all recipes"
+puts "    [2] See one recipe"
+puts "    [3] Create a new recipe"
+
+input_option = gets.chomp
+
+if input_option == "1"
+  response = Unirest.get("http://localhost:3000/recipes")
+  products = response.body
+  puts JSON.pretty_generate(products)
+elsif input_option == "2"
+  print "Enter recipe id: "
+  input_id = gets.chomp
+
+  response = Unirest.get("http://localhost:3000/recipes/#{input_id}")
+  product = response.body
+  puts JSON.pretty_generate(product)
+elsif input_option == "3"
+  client_params = {}
+
+  print "Title: "
+  client_params[:title] = gets.chomp
+
+  print "Chef: "
+  client_params[:chef] = gets.chomp
+
+  print "Ingredients: "
+  client_params[:ingredients] = gets.chomp
+
+  print "Directions: "
+  client_params[:directions] = gets.chomp
+
+  response = Unirest.post(
+                          "http://localhost:3000/recipes",
+                          parameters: client_params
+                          )
+  recipe_data = response.body
+
+  puts JSON.pretty_generate(recipe_data)
+end
+    
+
